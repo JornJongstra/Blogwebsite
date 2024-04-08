a245c4360a8726e66cb867de6c4c9130ef0f03e0
JornJongstra <94459310+JornJongstra@users.noreply.github.com>
2024-04-08 15:46:03 +0200

    classes gevuld

---
+++ Classes/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+	public class Blog
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Text { get; set; }
+		public DateTime CreatedDateTime { get; set; }
+		public List<Category> Categories { get; set; }
+		public List<Comment> Comments { get; set; }
+		public List<Like> Likes { get; set; }
+	}
+}
